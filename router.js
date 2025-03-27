8e3e5cd0bd69ab17c958fbce11e6fcc5e2777267
Tianna McHenry <mchenry.tianna@gmail.com>
2025-03-26 21:42:20 -0400

    Update router.js
    Updated file paths for get methods

--- router.js
+++ router.js
@@ -3,22 +3,22 @@ const path = require("path");
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/html', 'homepage.html'));
+  res.sendFile(path.join(__dirname, 'html', 'homepage.html'));
   //res.sendFile("html/index.html");
 });
 
 router.get('/about', function (req, res){
-  res.sendFile(path.join(__dirname, '/html', 'about.html'));
+  res.sendFile(path.join(__dirname, 'html', 'about.html'));
   //res.sendFile("html/about.html");
 });
 
 router.get('/contact', function (req, res){
-  res.sendFile(path.join(__dirname, '/html', 'contact.html'));
+  res.sendFile(path.join(__dirname, 'html', 'contact.html'));
   //res.sendFile("html/contact.html");
 });
 
 router.get('/gallery', function (req, res){
-  res.sendFile(path.join(__dirname, '/html', 'gallery.html'));
+  res.sendFile(path.join(__dirname, 'html', 'gallery.html'));
   //res.sendFile("html/gallery.html");
 });
 
