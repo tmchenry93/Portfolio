7efeabaf57add607c7e742b15ee220853eb9696e
Tianna McHenry <mchenry.tianna@gmail.com>
2025-03-26 21:32:14 -0400

    Update router.js
    Added absolute path and path module to script

--- router.js
+++ router.js
@@ -1,21 +1,26 @@
 const express = require('express');
+const path = require("path");
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.sendFile("html/index.html");
+  res.sendFile(path.join(__dirname, '/html', 'homepage.html'));
+  //res.sendFile("html/index.html");
 });
 
 router.get('/about', function (req, res){
-  res.sendFile("html/about.html");
+  res.sendFile(path.join(__dirname, '/html', 'about.html'));
+  //res.sendFile("html/about.html");
 });
 
 router.get('/contact', function (req, res){
-  res.sendFile("html/contact.html");
+  res.sendFile(path.join(__dirname, '/html', 'contact.html'));
+  //res.sendFile("html/contact.html");
 });
 
 router.get('/gallery', function (req, res){
-  res.sendFile("html/gallery.html");
+  res.sendFile(path.join(__dirname, '/html', 'gallery.html'));
+  //res.sendFile("html/gallery.html");
 });
 
 const app = express();
-app.use('/Portfolio', router);
+app.use('/', router);
