700fdff4db0e7bba1d624fb05163790b9dd289b6
Tianna McHenry <mchenry.tianna@gmail.com>
2025-03-27 17:41:08 -0400

    Update router.js
    Updated router for testing purposes

--- router.js
+++ router.js
@@ -3,24 +3,24 @@ const path = require("path");
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'html', 'homepage.html'));
-  //res.sendFile("html/index.html");
+  //res.sendFile(path.join(__dirname, 'html', 'homepage.html'));
+  res.send("this is the homepage");
 });
 
 router.get('/about', function (req, res){
-  res.sendFile(path.join(__dirname, 'html', 'about.html'));
-  //res.sendFile("html/about.html");
+  //res.sendFile(path.join(__dirname, 'html', 'about.html'));
+  res.send("this is the about page");
 });
 
 router.get('/contact', function (req, res){
-  res.sendFile(path.join(__dirname, 'html', 'contact.html'));
-  //res.sendFile("html/contact.html");
+  //res.sendFile(path.join(__dirname, 'html', 'contact.html'));
+  res.send("this is the contact page");
 });
 
 router.get('/gallery', function (req, res){
-  res.sendFile(path.join(__dirname, 'html', 'gallery.html'));
-  //res.sendFile("html/gallery.html");
+  //res.sendFile(path.join(__dirname, 'html', 'gallery.html'));
+  res.send("this is the gallery page");
 });
 
 const app = express();
-app.use('/', router);
+app.use(router);
