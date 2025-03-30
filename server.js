f317b7f4e869132daf060c0c352fd1a9cd61a649
tmchenry93 <mchenry.tianna@gmail.com>
2025-03-30 18:37:35 -0400

    removed router.js and condensed it to the server.js.

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+//const path = require("path");
+const router = express.Router();
+
+router.get('/', function (req, res) {
+  //res.sendFile(path.join(__dirname, 'html', 'homepage.html'));
+  res.send("this is the homepage");
+});
+
+router.get('/about', function (req, res){
+  //res.sendFile(path.join(__dirname, 'html', 'about.html'));
+  res.send("this is the about page");
+});
+
+router.get('/contact', function (req, res){
+  //res.sendFile(path.join(__dirname, 'html', 'contact.html'));
+  res.send("this is the contact page");
+});
+
+router.get('/gallery', function (req, res){
+  //res.sendFile(path.join(__dirname, 'html', 'gallery.html'));
+  res.send("this is the gallery page");
+});
+
+const app = express();
+app.use(router);
+var port = process.env.PORT || 3000;
+
+app.listen(port);
