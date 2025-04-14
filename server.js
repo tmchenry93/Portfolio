e62c68668804accdeef8f34309de030ec8b4ffdf
tmchenry93 <mchenry.tianna@gmail.com>
2025-04-14 00:23:00 -0400

    added a file path function to determine the absolute pathway. added a javascript and css injection script into the head of the document.

--- server.js
+++ server.js
@@ -2,24 +2,55 @@ const express = require('express');
 const path = require("path");
 const router = express.Router();
 
+function injectScript(scriptPath) {
+  const script = document.createElement('script');
+  script.src = scriptPath;
+  script.type = 'text/javascript';
+  document.head.appendChild(script);
+};
+
+function injectStyleSheet(cssPath) {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = cssPath;
+  document.head.appendChild(link);
+};
+
+function filePath(folderName, page){
+  return path.join(__dirname, folderName, page);
+};
+
 router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'html', 'homepage.html'));
-  //res.send("this is the homepage");
+  res.sendFile(filePath('html', 'homepage.html'));
+  injectScript(filePath("javascript", "global.js"));
+  injectStyleSheet(filePath("css", "global.css"));
+  injectScript(filePath("javascript", "homepage.js"));
+  injectStyleSheet(filePath("css", "homepage.css"));
 });
 
 router.get('/about', function (req, res){
-  res.sendFile(path.join(__dirname, 'html', 'about.html'));
-  //res.send("this is the about page");
+  res.sendFile(filePath('html', 'about.html'));
+  injectScript(filePath("javascript", "global.js"));
+  injectStyleSheet(filePath("css", "global.css"));
+  injectScript(filePath("javascript", "about.js"));
+  injectStyleSheet(filePath("css", "about.css"));
 });
 
 router.get('/contact', function (req, res){
-  res.sendFile(path.join(__dirname, 'html', 'contact.html'));
-  //res.send("this is the contact page");
+  res.sendFile(filePath('html', 'contact.html'));
+  injectScript(filePath("javascript", "global.js"));
+  injectStyleSheet(filePath("css", "global.css"));
+  injectScript(filePath("javascript", "contact.js"));
+  injectStyleSheet(filePath("css", "contact.css"));
 });
 
 router.get('/gallery', function (req, res){
-  res.sendFile(path.join(__dirname, 'html', 'gallery.html'));
-  //res.send("this is the gallery page");
+  res.sendFile(filePath('html', 'gallery.html'));
+  injectScript(filePath("javascript", "global.js"));
+  injectStyleSheet(filePath("css", "global.css"));
+  injectScript(filePath("javascript", "gallery.js"));
+  injectStyleSheet(filePath("css", "gallery.css"));
 });
 
 const app = express();
