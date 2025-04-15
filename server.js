b5769ece8dfb963b68a9a406b242cc8a536696ff
tmchenry93 <mchenry.tianna@gmail.com>
2025-04-15 01:57:08 -0400

    moved the image to the html pages. made changes to the css stylesheet.

--- server.js
+++ server.js
@@ -2,55 +2,20 @@ const express = require('express');
 const path = require("path");
 const router = express.Router();
 
-function injectScript(scriptPath) {
-  const script = document.createElement('script');
-  script.src = scriptPath;
-  script.type = 'text/javascript';
-  document.head.appendChild(script);
-};
-
-function injectStyleSheet(cssPath) {
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.type = 'text/css';
-  link.href = cssPath;
-  document.head.appendChild(link);
-};
-
-function filePath(folderName, page){
-  return path.join(__dirname, folderName, page);
-};
-
 router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'html', 'homepage.html'));
-  //injectScript(filePath("javascript", "global.js"));
-  //injectStyleSheet(filePath("css", "global.css"));
-  //injectScript(filePath("javascript", "homepage.js"));
-  //injectStyleSheet(filePath("css", "homepage.css"));
 });
 
 router.get('/about', function (req, res){
   res.sendFile(path.join(__dirname, 'html', 'about.html'));
-  //injectScript(filePath("javascript", "global.js"));
-  //injectStyleSheet(filePath("css", "global.css"));
-  //injectScript(filePath("javascript", "about.js"));
-  //injectStyleSheet(filePath("css", "about.css"));
 });
 
 router.get('/contact', function (req, res){
   res.sendFile(path.join(__dirname, 'html', 'contact.html'));
-  //injectScript(filePath("javascript", "global.js"));
-  //injectStyleSheet(filePath("css", "global.css"));
-  //injectScript(filePath("javascript", "contact.js"));
-  //injectStyleSheet(filePath("css", "contact.css"));
 });
 
 router.get('/gallery', function (req, res){
   res.sendFile(path.join(__dirname, 'html', 'gallery.html'));
-  //injectScript(filePath("javascript", "global.js"));
-  //injectStyleSheet(filePath("css", "global.css"));
-  //injectScript(filePath("javascript", "gallery.js"));
-  //injectStyleSheet(filePath("css", "gallery.css"));
 });
 
 const app = express();
