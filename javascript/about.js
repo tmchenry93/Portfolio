ca7c99caa66aa72ab2c3f731169dabe71c359311
Tianna McHenry <mchenry.tianna@gmail.com>
2026-02-03 11:34:38 -0500

    Update about.js
    Updated the toggle button for the resume display

--- javascript/about.js
+++ javascript/about.js
@@ -1,5 +1,5 @@
 // this button makes the resume div appear
 // this is a toggle class on the div
-$("#viewResume").click(function(){
-	$("#resume").toggle("fade");
+$("#resumeButton").click(function(){
+	$("#cvContainer").toggle(1500);
 });
