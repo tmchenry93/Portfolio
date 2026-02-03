cfabb8cd8adb6f25a766aa01dea676767722b04f
Tianna McHenry <mchenry.tianna@gmail.com>
2026-02-03 12:00:20 -0500

    Update about.js
    Displayed cv container when button is pressed

--- javascript/about.js
+++ javascript/about.js
@@ -1,5 +1,6 @@
 // this button makes the resume div appear
 // this is a toggle class on the div
 $("#resumeButton").click(function(){
+	$("#cvContainer").show();
 	$("#cvContainer").toggle(1500);
 });
