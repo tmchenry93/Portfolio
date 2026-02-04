891ad832b5547a642339efea09e91a8754cdbb9e
Tianna McHenry <mchenry.tianna@gmail.com>
2026-02-04 02:14:33 -0500

    Update about.js
    Updated function to toggle the button name

--- javascript/about.js
+++ javascript/about.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
      // this button makes the resume div appear
      // this is a toggle class on the div
      $("#resumeButton").click(function(){
+	     $("#cvContainer").toggle(500);
 		 if (document.getElementById("resumeButton").innerText === "Hide Resume"){
 			document.getElementById("resumeButton").innerText = "Display Resume";
+			return;
 		 };
-	     $("#cvContainer").toggle(500);
 		 document.getElementById("resumeButton").innerText = "Hide Resume";
+		 
 	 });
 });
