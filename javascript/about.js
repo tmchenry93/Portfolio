a1d10f2d513d3c52230d96e3ee2cd23b1659db3e
Tianna McHenry <mchenry.tianna@gmail.com>
2026-02-03 21:24:13 -0500

    Update about.js
    Created function to display CV and change button text

--- javascript/about.js
+++ javascript/about.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
      // this button makes the resume div appear
      // this is a toggle class on the div
      $("#resumeButton").click(function(){
-	     $("#cvContainer").show();
+		 var buttonText = document.getElementById("resumeButton").innerText;
 	     $("#cvContainer").toggle(1500);
+		 buttonText = "Hide Resume";
+		 if (buttonText = "Hide Resume"){
+			 $("#cvContainer").hide();
+			 buttonText = "Display Resume";
+		 };
 	 });
 });
