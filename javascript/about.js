dadf28422341e7cf6522f22600754dd5bf162d48
Tianna McHenry <mchenry.tianna@gmail.com>
2026-02-04 01:43:57 -0500

    Update about.js

--- javascript/about.js
+++ javascript/about.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
      // this button makes the resume div appear
      // this is a toggle class on the div
      $("#resumeButton").click(function(){
-		 if (document.getElementById("resumeButton").innerText = "Hide Resume"){
+		 if (document.getElementById("resumeButton").innerText === "Hide Resume"){
 			document.getElementById("resumeButton").innerText = "Display Resume";
 		 };
 	     $("#cvContainer").toggle(500);
