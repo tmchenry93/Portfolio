a59a64e1c6fc40c084d1f79ebccded8e7da9a56e
Tianna McHenry <mchenry.tianna@gmail.com>
2026-02-03 21:44:06 -0500

    Update about.js
    Changed the toggle time

--- javascript/about.js
+++ javascript/about.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
      // this button makes the resume div appear
      // this is a toggle class on the div
      $("#resumeButton").click(function(){
-		 var buttonText = document.getElementById("resumeButton").innerText;
-	     $("#cvContainer").toggle(1500);
-		 buttonText = "Hide Resume";
+		 document.getElementById("resumeButton").innerText;
+	     $("#cvContainer").toggle(500);
+		 document.getElementById("resumeButton").innerText = "Hide Resume";
 		 if (buttonText = "Hide Resume"){
 			 $("#cvContainer").hide();
 			 buttonText = "Display Resume";
