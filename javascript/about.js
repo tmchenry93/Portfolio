37e5a815a33898c693d6463ee71c00d8050c5ba2
Tianna McHenry <mchenry.tianna@gmail.com>
2026-02-03 22:41:10 -0500

    Update about.js
    Updated the function to change button text

--- javascript/about.js
+++ javascript/about.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
      // this button makes the resume div appear
      // this is a toggle class on the div
-	var buttonText = document.getElementById("resumeButton").innerText;
      $("#resumeButton").click(function(){
-		 if (buttonText = "Hide Resume"){
-			 buttonText = "Display Resume";
+		 if (document.getElementById("resumeButton").innerText = "Hide Resume"){
+			document.getElementById("resumeButton").innerText = "Display Resume";
 		 };
 	     $("#cvContainer").toggle(500);
-		 buttonText= "Hide Resume";
+		 document.getElementById("resumeButton").innerText = "Hide Resume";
 	 });
 });
