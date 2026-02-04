3b0669dafcf411610a4bc9a48cf959e8871ef493
Tianna McHenry <mchenry.tianna@gmail.com>
2026-02-03 20:59:51 -0500

    Update about.js
    Added the DOM ready function to this script

--- javascript/about.js
+++ javascript/about.js
@@ -1,6 +1,8 @@
-// this button makes the resume div appear
-// this is a toggle class on the div
-$("#resumeButton").click(function(){
-	$("#cvContainer").show();
-	$("#cvContainer").toggle(1500);
+$(document).ready(function(){
+     // this button makes the resume div appear
+     // this is a toggle class on the div
+     $("#resumeButton").click(function(){
+	     $("#cvContainer").show();
+	     $("#cvContainer").toggle(1500);
+	 });
 });
