9719bd82c81a2c6ebd0747dee4e7686ebf6a20bf
Tianna McHenry <mchenry.tianna@gmail.com>
2026-02-07 01:35:35 -0500

    Update about.js
    updated the resume button functionality to include whether or not the size of the screen large or small.  then it knows which cv container to toggle

--- javascript/about.js
+++ javascript/about.js
@@ -1,25 +1,21 @@
 $(document).ready(function(){
      // this button makes the resume div appear
      // this is a toggle class on the div
-	 if (window.innerWidth > 991.98px) {
-        $("#resumeButton").click(function(){
-	       $("#cvContainerLarge").toggle(500);
+     $("#resumeButton").click(function(){
+		if (window.innerWidth > 991.98) { 
+	 		$("#cvContainerLarge").toggle(500);
+			if (document.getElementById("resumeButton").innerText === "Hide Resume"){
+		    	document.getElementById("resumeButton").innerText = "Display Resume";
+			    return;
+		    };
+		    document.getElementById("resumeButton").innerText = "Hide Resume";
+		} else {
+		   $("#cvContainerSmall").toggle(500);
 		   if (document.getElementById("resumeButton").innerText === "Hide Resume"){
-		      document.getElementById("resumeButton").innerText = "Display Resume";
-			  return;
+		        document.getElementById("resumeButton").innerText = "Display Resume";
+			    return;
 		   };
 		   document.getElementById("resumeButton").innerText = "Hide Resume";
-		 
-	    });
-	 } else {
-		 $("#resumeButton").click(function(){
-	       $("#cvContainerSmall").toggle(500);
-		   if (document.getElementById("resumeButton").innerText === "Hide Resume"){
-		      document.getElementById("resumeButton").innerText = "Display Resume";
-			  return;
-		   };
-		   document.getElementById("resumeButton").innerText = "Hide Resume";
-		 
-	    });
-	 }
+		}
+	});
 });
