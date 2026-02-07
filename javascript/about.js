45d63235ee5ea36260bc7c03783205da590a886f
Tianna McHenry <mchenry.tianna@gmail.com>
2026-02-07 00:50:47 -0500

    Update about.js
    Updated the id names to reflect which cv container toggles. Updated the conditional to include the toggle back and forth from cv container large and small

--- javascript/about.js
+++ javascript/about.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
      // this button makes the resume div appear
      // this is a toggle class on the div
-     $("#resumeButton").click(function(){
-	     $("#cvContainer").toggle(500);
-		 if (document.getElementById("resumeButton").innerText === "Hide Resume"){
-			document.getElementById("resumeButton").innerText = "Display Resume";
-			return;
-		 };
-		 document.getElementById("resumeButton").innerText = "Hide Resume";
+	 if (document.documentElement.clientWidth > 991.98px) {
+        $("#resumeButton").click(function(){
+	       $("#cvContainerLarge").toggle(500);
+		   if (document.getElementById("resumeButton").innerText === "Hide Resume"){
+		      document.getElementById("resumeButton").innerText = "Display Resume";
+			  return;
+		   };
+		   document.getElementById("resumeButton").innerText = "Hide Resume";
 		 
-	 });
+	    });
+	 } else {
+		 $("#resumeButton").click(function(){
+	       $("#cvContainerSmall").toggle(500);
+		   if (document.getElementById("resumeButton").innerText === "Hide Resume"){
+		      document.getElementById("resumeButton").innerText = "Display Resume";
+			  return;
+		   };
+		   document.getElementById("resumeButton").innerText = "Hide Resume";
+		 
+	    });
+	 }
 });
