cd315c4d46fb153995178cf3da185118378e8ff3
Tianna McHenry <mchenry.tianna@gmail.com>
2026-02-07 00:58:07 -0500

    Update about.js
    Updated the inner width property name

--- javascript/about.js
+++ javascript/about.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
      // this button makes the resume div appear
      // this is a toggle class on the div
-	 if (document.documentElement.clientWidth > 991.98px) {
+	 if (window.innerWidth > 991.98px) {
         $("#resumeButton").click(function(){
 	       $("#cvContainerLarge").toggle(500);
 		   if (document.getElementById("resumeButton").innerText === "Hide Resume"){
